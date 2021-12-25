734a914ff4cb031e43fd98a6cf94e6e46a7bb318
Aykhan <Aykhan.Alifov@atltech.com>
2021-12-25 16:33:42 +0400

    Create DataContext

---
+++ WebApplication1/ApplicationDataContext/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.ApplicationDataContext
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}

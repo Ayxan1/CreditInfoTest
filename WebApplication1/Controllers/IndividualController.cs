17d7e13e9e00bd6b39344a9386665ba9c0ce8ffe
Aykhan <Aykhan.Alifov@atltech.com>
2021-12-25 19:16:13 +0400

    Create Individual controller

---
+++ WebApplication1/Controllers/IndividualController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/v1/")]
+    public class IndividualController : ControllerBase
+    {
+        private readonly IIndividualService individualService;
+
+        public IndividualController(IIndividualService individualService)
+        {
+            this.individualService = individualService;
+        }
+
+        [HttpGet("individuals/{nationalId}")]
+        public async Task<IActionResult> Get(string nationalId)
+        {
+            var result = await individualService.Get(nationalId);
+
+            if(result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+    }
+}
